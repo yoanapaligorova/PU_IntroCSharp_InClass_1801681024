4616437abb19b307041248bde801b679dc9f921a
hrisnedqlkova <joanapal17@gmail.com>
2018-10-16 15:48:50 +0300

    Added to in class

--- W3InClass/MusicFactory/Program.cs
+++ W3InClass/MusicFactory/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,33 @@ namespace MusicFactory
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Play somemusic..");
+			Console.WriteLine("Do you want to play some music ?");
+			string answer = Console.ReadLine();
+			if (answer == "Yes")
+			{
+				using (SoundPlayer player = new SoundPlayer(@"C:\Users\fmi\Desktop\w3\PU_IntroCSharp_InClass_1801681024\W3InClass\MusicFactory\music"))
+				{
+
+					Console.WriteLine("Now plying ...");
+					player.PlaySync();
+					while (true)
+					{
+						Console.Write(".");
+					}
+				}
+					
+			}
+			else if (answer == "No")
+			{
+				Console.WriteLine("Goodbye!");
+			}
+			else
+			{
+				Console.WriteLine("Wrong input");
+			}
+
+			
+
 		}
 	}
 }
