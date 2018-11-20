5a693e14c865de89b088b2c73116a257b6aa93a6
radkotodev <joanapal17@gmail.com>
2018-11-20 16:21:11 +0200

    WEEEK 8

---
+++ W3InClass/PiggyBank/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiggyBank
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int days = 30;
+			int saving = 2; //lewa
+			int throwAwayForFalseVodka = 5;
+			int monthsInYear = 12;
+			double result = 0;
+
+			try
+			{
+				int tankPrice = int.Parse(Console.ReadLine());
+				int partyDays = int.Parse(Console.ReadLine());
+
+				int daysWithoutVodka = days - partyDays;
+				int totalMonthSavings = daysWithoutVodka * saving;
+				int netSaving = totalMonthSavings - (throwAwayForFalseVodka * partyDays);
+
+				double monthsForSaving4tank = tankPrice / netSaving;
+
+				result = monthsForSaving4tank / monthsInYear;
+
+			}
+			catch (Exception ex)
+			{
+
+				Console.WriteLine(ex.Message);
+				Console.WriteLine("Please try again ");
+
+			}
+			if (result < 0) Console.WriteLine("Never");
+			else
+			{
+				result = Math.Round(result, 1, MidpointRounding.AwayFromZero);
+				string months = result.ToString().Split(',')[1];
+				Console.WriteLine($"{(int)result}years{months} months ");
+
+			}
+
+
+
+
+
+		}
+	}
+}
---
+++ W3InClass/TextBombardment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TextBombardment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			Console.WriteLine("Enter the sntence you wish to bombard");
+			string sentence = Console.ReadLine();
+			try
+			{
+				int lineWidth = int.Parse(Console.ReadLine());
+				string bombIndex = Console.ReadLine();
+				Console.WriteLine(sentence);
+				Console.WriteLine(lineWidth);
+				Console.WriteLine(bombIndex);
+
+				double rows = sentence.Length /lineWidth   ;
+				
+	
+					for(int i=0;i<sentence.Length;i++)
+					{
+						Console.Write(sentence[i]);
+						if (i == lineWidth)
+						{
+							Console.Write("\n");
+						}
+
+					}
+					 
+				Console.WriteLine();
+				
+
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+
+			}
+        } 
+    }
+}
